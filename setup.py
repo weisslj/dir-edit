3d38c56a9c75964af2396fff68d47974624930fa
Johannes Weißl <jargon@molb.org>
2010-06-09 09:46:03 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='dir-edit',
+      version='1.0',
+      description='Chooses a file from a directory. '\
+                  'Very handy to re-watch tv series!',
+      author='Johannes Weißl',
+      author_email='jargon@molb.org',
+      url='http://github.com/weisslj/dir-edit',
+      license='GNU GPL v3',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python :: 2.5',
+          'Topic :: Utilities',
+      ],
+      scripts=['dir_edit'])
