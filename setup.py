aebf591d670abe231cc23170cb8a5d3cacda508e
Johannes Weißl <jargon@molb.org>
2017-02-22 11:42:42 +0100

    Cosmetics

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""A setuptools based setup module."""
 
 from distutils.core import setup
 
-setup(name='dir-edit',
-      version='1.1',
-      description='Rename or remove files in a directory using an editor (e.g. vi)',
-      author=u'Johannes Weißl',
-      author_email='jargon@molb.org',
-      url='http://github.com/weisslj/dir-edit',
-      license='GNU GPL v3',
-      classifiers=[
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Programming Language :: Python :: 2.5',
-          'Topic :: Utilities',
-      ],
-      scripts=['dir_edit'])
+setup(
+    name='dir-edit',
+    version='1.1',
+    description='Rename or remove files in a directory using an editor (e.g. vi)',
+    author=u'Johannes Weißl',
+    author_email='jargon@molb.org',
+    url='http://github.com/weisslj/dir-edit',
+    license='GNU GPL v3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Utilities',
+    ],
+    scripts=['dir_edit'],
+)
