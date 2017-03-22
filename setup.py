7eb8f0c0d808952fad77719650aadaf324188989
Johannes Weißl <jargon@molb.org>
2017-03-22 04:04:59 +0100

    Include long description in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@
 """A setuptools based setup module."""
 
 from __future__ import unicode_literals
+from os import path
 from setuptools import setup
+
+# Get the long description from the README file
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as stream:
+    LONG_DESCRIPTION = stream.read()
 
 setup(
     name='dir-edit',
     version='1.1',
     description='Rename or remove files in a directory using an editor (e.g. vi)',
+    long_description=LONG_DESCRIPTION,
     author='Johannes Weißl',
     author_email='jargon@molb.org',
     url='http://github.com/weisslj/dir-edit',
